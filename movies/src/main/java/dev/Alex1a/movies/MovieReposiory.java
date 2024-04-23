4fd5741849083fc71067e28c526d71d2ade972e6
AlexiaBojian <bojianalexia@icloud.com>
2024-04-23 13:58:07 +0200

    Sort MOVIES by indbID

--- movies/src/main/java/dev/Alex1a/movies/MovieReposiory.java
+++ movies/src/main/java/dev/Alex1a/movies/MovieReposiory.java
@@ -4,6 +4,9 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MovieReposiory extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByimdbId(String imdbId);
 }
--- movies/src/main/java/dev/Alex1a/movies/MovieService.java
+++ movies/src/main/java/dev/Alex1a/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.Alex1a.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +15,9 @@ public class MovieService {
     private MovieReposiory movieReposiory;
     public List<Movie> allMovies(){
         return movieReposiory.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieReposiory.findMovieByimdbId(imdbId);
     }
 }
